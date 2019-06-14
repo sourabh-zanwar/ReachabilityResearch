8c4a932daccc5ffe7f1ff7cddd19a9b17c5b830d
sourabhznwr <31401578+sourabhznwr@users.noreply.github.com>
2019-06-14 18:01:59 +0200

    Update handedness.java

--- app/src/main/java/com/example/reachabilityresearch/handedness.java
+++ app/src/main/java/com/example/reachabilityresearch/handedness.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class handedness extends AppCompatActivity {
 
     Button left,right;
+    private static final String TAG = "Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,7 @@ public class handedness extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent3 = new Intent(handedness.this,leftscreen.class);
+                Log.d(TAG, "Choose Strong Hand : Clicked Button LEFT");
                 startActivity(intent3);
             }
         });
@@ -30,8 +33,8 @@ public class handedness extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent4 = new Intent(handedness.this,rightscreen.class);
+                Log.d(TAG, "Choose Strong Hand : Clicked Button 1");
                 startActivity(intent4);
-                finish();
             }
         });
     }
